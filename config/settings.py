810e925bc5bb7c62531c57fe0217da00bb9485db
Tmahn001 <toluiwalewa@gmail.com>
2023-05-17 11:02:40 +0100

    craete-link and csrf-fix

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,4 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://*.web-production-467b.up.railway.app','https://*.127.0.0.1']
